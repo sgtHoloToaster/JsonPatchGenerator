044e7f5f02ee8695b5a1b325547fdb195dffd141
Mikhail Podolskiy <edge769@yandex.ru>
2020-01-04 22:19:56 +0300

    add tests for simple remove operations

---
+++ tests/JsonPatchGenerator.Core.Tests/Tests/JsonPatchGeneratorRemoveTests.cs
@@ -0,0 +1,52 @@
+﻿using JsonPatchGenerator.Core.Services;
+using JsonPatchGenerator.Core.Tests.Models;
+using JsonPatchGenerator.Interface.Models;
+using System;
+using Xunit;
+
+namespace JsonPatchGenerator.Core.Tests.Tests
+{
+    public class JsonPatchGeneratorRemoveTests : JsonPatchGeneratorTests
+    {
+        public JsonPatchGeneratorRemoveTests() : base() { }
+
+        [Fact]
+        public void SupportSimpleTypeArrayRemoveOperation() =>
+            TestSimpleTypeArrayRemoveOperation(HasRemoveOperation);
+
+        [Fact]
+        public void SimpleTypeArrayRemoveOperationHasCorrectPath() =>
+            TestSimpleTypeArrayRemoveOperation(HasCorrectPath);
+
+        [Fact]
+        public void SimpleTypeArrayRemoveOperationHasCorrectValue() =>
+            TestSimpleTypeArrayRemoveOperation(HasCorrectValue);
+
+        [Fact]
+        public void SimpleTypeArrayMoveDoesntProduceExtraOperations() =>
+            TestSimpleTypeArrayRemoveOperation(HasNoExtraOperations);
+
+        private void TestSimpleTypeArrayRemoveOperation(AssertAction assert)
+        {
+            // arrange
+            const int removedValue = 2;
+            var first = new ComplexPropertiesModel { SimpleTypeArray = new[] { 1, removedValue, 3, 4,  } };
+            var second = new ComplexPropertiesModel { SimpleTypeArray = new[] { 1, 3, 4 } };
+            var expectedPath = $"/{nameof(ComplexPropertiesModel.SimpleTypeArray)}/{Array.IndexOf(first.SimpleTypeArray, removedValue)}";
+
+            var target = Mocker.Create<JsonPatchGeneratorService>();
+
+            // act
+            var result = target.GetDiff(first, second);
+
+            // assert
+            assert(result, expectedPath, removedValue);
+        }
+
+        private void HasRemoveOperation(DiffDocument result) =>
+            HasOperation(result, OperationType.Remove);
+
+        private void HasRemoveOperation(DiffDocument result, string path, object newValue) =>
+            HasRemoveOperation(result);
+    }
+}
