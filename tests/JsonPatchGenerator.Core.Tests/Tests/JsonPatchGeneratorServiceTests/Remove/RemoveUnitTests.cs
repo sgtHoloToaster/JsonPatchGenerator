ccc1190c2bdc9d45fde01fa6a1d9ba3272a3eb03
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-08 15:44:43 +0300

    add remove operation unit tests

---
+++ tests/JsonPatchGenerator.Core.Tests/Tests/JsonPatchGeneratorServiceTests/Remove/RemoveUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoMoqCore;
+using JsonPatchGenerator.Core.Services;
+using JsonPatchGenerator.Interface.Enums;
+using JsonPatchGenerator.Interface.Models;
+using JsonPatchGenerator.Interface.Services;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace JsonPatchGenerator.Core.Tests.Tests.JsonPatchGeneratorServiceTests
+{
+    public class RemoveUnitTests : IRemoveTests
+    {
+        readonly AutoMoqer _mocker = new AutoMoqer();
+        readonly RemoveTestsBase _base;
+
+        public RemoveUnitTests()
+        {
+            var operations = new List<Operation>();
+            _mocker.GetMock<IPatchDocumentBuilder>()
+                .Setup(m => m.AppendRemoveOperation(It.IsAny<string>()))
+                .Callback<string>(path => operations.Add(new Operation(OperationType.Remove, path)));
+            _mocker.GetMock<IPatchDocumentBuilder>()
+                .Setup(m => m.Build())
+                .Returns(new PatchDocument(operations));
+            _mocker.GetMock<IPatchDocumentBuilderFactory>()
+                .Setup(m => m.Create())
+                .Returns(() => _mocker.Create<IPatchDocumentBuilder>());
+            _base = new RemoveTestsBase(GetTarget);
+        }
+
+        private JsonPatchGeneratorService GetTarget() =>
+            _mocker.Create<JsonPatchGeneratorService>();
+
+        [Fact]
+        public void SimpleTypeArrayMoveDoesntProduceExtraOperations() =>
+            _base.SimpleTypeArrayMoveDoesntProduceExtraOperations();
+
+        [Fact]
+        public void SimpleTypeArrayRemoveOperationHasCorrectPath() =>
+            _base.SimpleTypeArrayRemoveOperationHasCorrectPath();
+
+        [Fact]
+        public void SimpleTypeArrayRemoveOperationHasCorrectValue() =>
+            _base.SimpleTypeArrayRemoveOperationHasCorrectValue();
+
+        [Fact]
+        public void SupportSimpleTypeArrayRemoveOperation() =>
+            _base.SupportSimpleTypeArrayRemoveOperation();
+    }
+}
