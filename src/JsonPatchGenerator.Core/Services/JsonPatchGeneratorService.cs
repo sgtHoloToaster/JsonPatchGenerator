f465db3ab67619154fa8782ea03d4350120a0db7
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-08 16:57:15 +0300

    make JsonPatchGeneratorService dependent on interfaces instead of concrete realizations

--- src/JsonPatchGenerator.Core/Services/JsonPatchGeneratorService.cs
+++ src/JsonPatchGenerator.Core/Services/JsonPatchGeneratorService.cs
@@ -22,45 +22,43 @@ namespace JsonPatchGenerator.Core.Services
             _patchDocumentBuilderFactory = patchDocumentBuilderFactory;
         }
 
-        public IPatchDocument GetDiff(object first, object second) =>
-            new PatchDocument(GetObjectPatchOperations(first, second, string.Empty)); // TODO: replace with the abstract builder
-
-        private IEnumerable<Operation> GetObjectPatchOperations(object first, object second, string path) =>
-            GetObjectPatchOperations(first, second, path, first.GetType());
-
-        private IEnumerable<Operation> GetObjectPatchOperations(object first, object second, string path, Type type)
+        public IPatchDocument GetDiff(object first, object second)
         {
-            var operations = new List<Operation>();
+            var builder = _patchDocumentBuilderFactory.Create();
+            GetObjectPatchOperations(builder, first, second, string.Empty);
+            return builder.Build();
+        }
+
+        private void GetObjectPatchOperations(IPatchDocumentBuilder builder, object first, object second, string path) =>
+            GetObjectPatchOperations(builder, first, second, path, first.GetType());
+
+        private void GetObjectPatchOperations(IPatchDocumentBuilder builder, object first, object second, string path, Type type)
+        {
             var properties = _typeResolver.GetProperties(type);
             foreach (var property in properties)
             {
                 var firstValue = property.GetValue(first, _typeResolver);
                 var secondValue = property.GetValue(second, _typeResolver);
-                operations.AddRange(GetValuesDiff(firstValue, secondValue, ConcatPath(path, property.Name), property.Type));
+                GetValuesDiff(builder, firstValue, secondValue, ConcatPath(path, property.Name), property.Type);
             }
-
-            return operations;
         }
 
-        private IEnumerable<Operation> GetValuesDiff(object firstValue, object secondValue, string path, Type propertyType)
+        private void GetValuesDiff(IPatchDocumentBuilder builder, object firstValue, object secondValue, string path, Type propertyType)
         {
             if (firstValue != null && secondValue != null && !propertyType.IsPrimitive)
             {
                 if (propertyType.IsArray)
-                    return GetArrayPatchOperations(firstValue as Array, secondValue as Array, path, propertyType);
+                    GetArrayPatchOperations(builder, firstValue as Array, secondValue as Array, path, propertyType);
                 else
-                    return GetObjectPatchOperations(firstValue, secondValue, path, propertyType);
+                    GetObjectPatchOperations(builder, firstValue, secondValue, path, propertyType);
             }
             else if (!ReferenceEquals(firstValue, secondValue) && (!firstValue?.Equals(secondValue) ?? true))
-                return new[] { new Operation(OperationType.Replace, path, secondValue) };
-            else
-                return Enumerable.Empty<Operation>();
+                builder.AppendReplaceOperation(path, secondValue);
         }
 
         //TODO: refactor
-        private IEnumerable<Operation> GetArrayPatchOperations(Array firstArray, Array secondArray, string path, Type propertyType)
+        private void GetArrayPatchOperations(IPatchDocumentBuilder builder, Array firstArray, Array secondArray, string path, Type propertyType)
         {
-            var operations = new List<Operation>();
             var firstArrayHashCodes = new ArrayHashIndexMap(firstArray, _typeResolver.GetHashCode);
             var secondArrayHashCodes = new ArrayHashIndexMap(secondArray, _typeResolver.GetHashCode);
             var toAdd = secondArrayHashCodes.Except(firstArrayHashCodes).ToArray();
@@ -74,7 +72,7 @@ namespace JsonPatchGenerator.Core.Services
                     var index = rawIndex + offsets[rawIndex];
                     if (index >= firstArray.Length || !toAdd.Contains(secondArrayHashCodes[index]))
                     {
-                        operations.Add(new Operation(OperationType.Remove, ConcatPath(path, index), firstArray.GetValue(index)));
+                        builder.AppendRemoveOperation(ConcatPath(path, index));
                         for (var i = index + 1; i < offsets.Length; i++)
                             offsets[i]--;
                     }
@@ -88,16 +86,14 @@ namespace JsonPatchGenerator.Core.Services
                     continue;
 
                 if (index >= firstArray.Length)
-                {
-                    operations.Add(new Operation(OperationType.Add, ConcatPath(path, ArrayLastPositionLiteral), secondArray.GetValue(index)));
-                }
+                    builder.AppendAddOperation(ConcatPath(path, ArrayLastPositionLiteral), secondArray.GetValue(index));
                 else if (toRemove.Contains(firstArrayHashCodes[indexWithOffset]) && toAdd.Contains(secondArrayHashCodes[index]))
                 {
                     var firstArrayValue = firstArray.GetValue(indexWithOffset);
                     var secondArrayValue = secondArray.GetValue(index);
                     var currentPath = ConcatPath(path, index);
                     var elementType = propertyType.GetElementType();
-                    operations.AddRange(GetValuesDiff(firstArrayValue, secondArrayValue, currentPath, elementType));
+                    GetValuesDiff(builder, firstArrayValue, secondArrayValue, currentPath, elementType);
                 }
                 else if (firstArrayHashCodes.Map.TryGetValue(secondArrayHashCodes[index], out var indexes))
                 {
@@ -109,7 +105,7 @@ namespace JsonPatchGenerator.Core.Services
                     if (fromIndex == index)
                         continue;
 
-                    operations.Add(new Operation(OperationType.Move, ConcatPath(path, index), secondArray.GetValue(index), ConcatPath(path, fromIndex)));
+                    builder.AppendMoveOperation(ConcatPath(path, index), ConcatPath(path, fromIndex));
                     if (fromIndex > index)
                         for (var i = index + 1; i <= fromIndex; i++)
                             offsets[i]--;
@@ -119,13 +115,11 @@ namespace JsonPatchGenerator.Core.Services
                 }
                 else
                 {
-                    operations.Add(new Operation(OperationType.Add, ConcatPath(path, index), secondArray.GetValue(index)));
+                    builder.AppendAddOperation(ConcatPath(path, index), secondArray.GetValue(index));
                     for (var i = index + 1; i < offsets.Length; i++)
                         offsets[i]--;
                 }
             }
-
-            return operations;
         }
 
         private string ConcatPath(params object[] pathParts) =>
