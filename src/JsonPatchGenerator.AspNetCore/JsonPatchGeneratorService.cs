def3565611c9379b0f9c0e1c6a0c4645d97d1098
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-08 17:54:32 +0300

    add an assembly for AspNetCore integration

---
+++ src/JsonPatchGenerator.AspNetCore/JsonPatchGeneratorService.cs
@@ -0,0 +1,14 @@
+﻿using JsonPatchGenerator.Interface.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using System;
+
+namespace JsonPatchGenerator.AspNetCore
+{
+    public class JsonPatchGeneratorService : IJsonPatchGenerator<IJsonPatchDocument>
+    {
+        public IJsonPatchDocument Generate(object first, object second)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
