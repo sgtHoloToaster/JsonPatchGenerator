26e7c4b2c9b005aee5cc69072b1906fb6e6deb6c
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-08 15:32:03 +0300

    add unit tests for JsonPatchGenerator add operations

---
+++ tests/JsonPatchGenerator.Core.Tests/Tests/JsonPatchGeneratorServiceTests/Add/AddUnitTests.cs
@@ -0,0 +1,63 @@
+﻿using AutoMoqCore;
+using JsonPatchGenerator.Core.Services;
+using JsonPatchGenerator.Interface.Enums;
+using JsonPatchGenerator.Interface.Models;
+using JsonPatchGenerator.Interface.Services;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace JsonPatchGenerator.Core.Tests.Tests.JsonPatchGeneratorServiceTests
+{
+    public class AddUnitTests : IAddTests
+    {
+        readonly AutoMoqer _mocker = new AutoMoqer();
+        readonly AddTestsBase _base;
+
+        public AddUnitTests()
+        {
+            var operations = new List<Operation>();
+            _mocker.GetMock<IPatchDocumentBuilder>()
+                .Setup(m => m.AppendAddOperation(It.IsAny<string>(), It.IsAny<object>()))
+                .Callback<string, object>((path, value) => operations.Add(new Operation(OperationType.Add, path, value)));
+            _mocker.GetMock<IPatchDocumentBuilder>()
+                .Setup(m => m.Build())
+                .Returns(new PatchDocument(operations));
+            _mocker.GetMock<IPatchDocumentBuilderFactory>()
+                .Setup(m => m.Create())
+                .Returns(() => _mocker.Create<IPatchDocumentBuilder>());
+            _base = new AddTestsBase(GetTarget);
+        }
+
+        private JsonPatchGeneratorService GetTarget() =>
+            _mocker.Create<JsonPatchGeneratorService>();
+
+        [Fact]
+        public void SimpleTypeArrayAddOperationHasCorrectPath() =>
+            _base.SimpleTypeArrayAddOperationHasCorrectPath();
+
+        [Fact]
+        public void SimpleTypeArrayAddOperationHasCorrectValue() =>
+            _base.SimpleTypeArrayAddOperationHasCorrectValue();
+
+        [Fact]
+        public void SimpleTypeArrayIndexBasedAddDoesntProduceExtraOperations() =>
+            _base.SimpleTypeArrayIndexBasedAddDoesntProduceExtraOperations();
+
+        [Fact]
+        public void SimpleTypeArrayIndexBasedAddOperationHasCorrectPath() =>
+            _base.SimpleTypeArrayIndexBasedAddOperationHasCorrectPath();
+
+        [Fact]
+        public void SimpleTypeArrayIndexBasedAddOperationHasCorrectValue() =>
+            _base.SimpleTypeArrayIndexBasedAddOperationHasCorrectValue();
+
+        [Fact]
+        public void SupportSimpleTypeArrayAddOperation() =>
+            _base.SupportSimpleTypeArrayAddOperation();
+
+        [Fact]
+        public void SupportSimpleTypeArrayIndexBasedAddOperation() =>
+            _base.SupportSimpleTypeArrayIndexBasedAddOperation();
+    }
+}
