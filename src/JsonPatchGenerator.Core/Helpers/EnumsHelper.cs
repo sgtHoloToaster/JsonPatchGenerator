e10d8f9ae835727e0df0c279086025c6eba751c5
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-20 21:38:21 +0300

    add a method that gives an ability to get an enum value by the value of its EnumMember attribute

---
+++ src/JsonPatchGenerator.Core/Helpers/EnumsHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace JsonPatchGenerator.Core.Helpers
+{
+    public static class EnumsHelper
+    {
+        public static T GetValueByEnumMemberAttribute<T>(string attributeValue) where T : Enum
+        {
+            var type = typeof(T);
+            foreach (var field in type.GetFields())
+            {
+                if (Attribute.GetCustomAttribute(field, typeof(EnumMemberAttribute)) is EnumMemberAttribute attribute)
+                {
+                    if (Equals(attribute.Value, attributeValue))
+                        return (T)field.GetValue(null);
+                }
+            }
+            throw new ArgumentException("Not found", nameof(attributeValue));
+        }
+    }
+}
---
+++ tests/JsonPatchGenerator.Core.Tests/Tests/EnumsHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using JsonPatchGenerator.Core.Helpers;
+using JsonPatchGenerator.Interface.Enums;
+using System;
+using Xunit;
+
+namespace JsonPatchGenerator.Core.Tests.Tests
+{
+    public class EnumsHelperTests
+    {
+        [Theory]
+        [InlineData("add", OperationType.Add)]
+        [InlineData("remove", OperationType.Remove)]
+        [InlineData("test", OperationType.Test)]
+        public void ReturnsEnumValueForEnumMemberAttributeValue(string input, OperationType expected)
+        {
+            // act
+            var result = EnumsHelper.GetValueByEnumMemberAttribute<OperationType>(input);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ThrowsArgumentExceptionWhenUnknownValueIsPassed()
+        {
+            // arrange
+            const string input = "someEnumMemberThatDoesNotForSureExist";
+
+            // act & assert
+            Assert.Throws<ArgumentException>(() => EnumsHelper.GetValueByEnumMemberAttribute<OperationType>(input));
+        }
+    }
+}
