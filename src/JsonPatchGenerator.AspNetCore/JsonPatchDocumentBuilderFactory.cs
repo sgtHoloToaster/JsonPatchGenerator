e742e67b26998a41db117d5bbc062c88fa3aead0
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-21 23:34:52 +0300

    add an implementation for JsonPatchDocumentBuilderFactory

--- src/JsonPatchGenerator.AspNetCore/JsonPatchDocumentBuilderFactory.cs
+++ src/JsonPatchGenerator.AspNetCore/JsonPatchDocumentBuilderFactory.cs
@@ -1,14 +1,11 @@
 ﻿using JsonPatchGenerator.AspNetCore.Abstract;
 using JsonPatchGenerator.Interface.Services;
-using System;
 
 namespace JsonPatchGenerator.AspNetCore
 {
     public class JsonPatchDocumentBuilderFactory : IPatchDocumentBuilderFactory<IJsonPatchDocumentWrapper>
     {
-        public IPatchDocumentBuilder<IJsonPatchDocumentWrapper> Create()
-        {
-            throw new NotImplementedException();
-        }
+        public IPatchDocumentBuilder<IJsonPatchDocumentWrapper> Create() =>
+            new JsonPatchDocumentBuilder();
     }
 }
