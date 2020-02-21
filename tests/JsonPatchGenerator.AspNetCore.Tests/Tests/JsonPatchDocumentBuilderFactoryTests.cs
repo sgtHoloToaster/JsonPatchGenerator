c948666ba6bef75314e5fa674addafc322e3a841
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-21 23:33:15 +0300

    add tests for JsonPatchDocumentBuilderFactory

---
+++ tests/JsonPatchGenerator.AspNetCore.Tests/Tests/JsonPatchDocumentBuilderFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using JsonPatchGenerator.AspNetCore;
+using JsonPatchGenerator.Interface.Models.Abstract;
+using JsonPatchGenerator.Interface.Services;
+using System;
+using Xunit;
+
+namespace JsonPatchGenerator.AspNetCore.Tests.Tests
+{
+    public class JsonPatchDocumentBuilderFactoryTests
+    {
+        [Fact]
+        public void CanCreateBuilder() =>
+            TestBuilderCreate(Assert.NotNull);
+
+        [Fact]
+        public void CreatedBuilderHasCorrectType() =>
+            TestBuilderCreate(result => Assert.True(result is JsonPatchDocumentBuilder));
+
+        private void TestBuilderCreate(Action<IPatchDocumentBuilder<IPatchDocument>> assert)
+        {
+            // arrange
+            var target = new JsonPatchDocumentBuilderFactory();
+
+            // act
+            var result = target.Create();
+
+            // assert
+            assert(result);
+        }
+    }
+}
