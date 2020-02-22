0d71764003f7ea1ce458bfee687dba50e31edc6e
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-22 20:23:13 +0300

    rename the JsonPatchDocumentGeneratorService

---
+++ src/JsonPatchGenerator.AspNetCore/JsonPatchDocumentGeneratorService.cs
@@ -0,0 +1,23 @@
+﻿using JsonPatchGenerator.AspNetCore.Abstract;
+using JsonPatchGenerator.Interface.Models.Abstract;
+using JsonPatchGenerator.Interface.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using System;
+
+namespace JsonPatchGenerator.AspNetCore
+{
+    public class JsonPatchDocumentGeneratorService : IJsonPatchGenerator<IJsonPatchDocument>
+    {
+        readonly IJsonPatchGenerator<IJsonPatchDocumentWrapper> _patchGenerator;
+
+        public JsonPatchDocumentGeneratorService(IJsonPatchGenerator<IJsonPatchDocumentWrapper> jsonPatchGenerator)
+        {
+            _patchGenerator = jsonPatchGenerator;
+        }
+
+        public IJsonPatchDocument Generate(object first, object second)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
