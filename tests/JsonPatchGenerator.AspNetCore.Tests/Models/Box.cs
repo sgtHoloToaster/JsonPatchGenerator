9cd3e59dcd8d5f4d52eb7aa627a429da1886f9c1
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-22 20:23:32 +0300

    implement unit tests for JsonPatchDocumentGeneratorService

---
+++ tests/JsonPatchGenerator.AspNetCore.Tests/Models/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JsonPatchGenerator.AspNetCore.Tests.Models
+{
+    public class Box
+    {
+        public string Title { get; set; }
+
+        public int Id { get; set; }
+
+        public List<Box> Inside { get; set; } = new List<Box>();
+
+        public override bool Equals(object obj) =>
+            obj is Box box &&
+                   Title == box.Title &&
+                   Id == box.Id &&
+                   EqualityComparer<List<Box>>.Default.Equals(Inside, box.Inside);
+
+        public override int GetHashCode() => 
+            HashCode.Combine(Title, Id, Inside);
+    }
+}
---
+++ tests/JsonPatchGenerator.AspNetCore.Tests/Tests/JsonPatchDocumentGenerator/UnitTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoMoqCore;
+using JsonPatchGenerator.AspNetCore.Abstract;
+using JsonPatchGenerator.AspNetCore.Tests.Models;
+using JsonPatchGenerator.Interface.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using Xunit;
+
+namespace JsonPatchGenerator.AspNetCore.Tests.Tests.JsonPatchDocumentGenerator
+{
+    public class UnitTests
+    {
+        readonly AutoMoqer _mocker = new AutoMoqer();
+
+        [Fact]
+        public void CanGenerateDocument()
+        {
+            // arrange
+            var first = new object();
+            var second = new object();
+            var expected = new JsonPatchDocument();
+            _mocker.GetMock<IJsonPatchGenerator<IJsonPatchDocumentWrapper>>()
+                .Setup(m => m.Generate(first, second))
+                .Returns(new JsonPatchDocumentWrapper(expected));
+            var target = _mocker.Create<JsonPatchDocumentGeneratorService>();
+
+            // act
+            var result = target.Generate(first, second);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GeneratedDocumentHasCorrectOperations()
+        {
+            // arrange 
+            var first = new Box { Id = 1, Title = "OldBox" };
+            var second = new Box { Id = 2, Title = "NewBox" };
+            var expectedOperations = new List<Operation>
+            {
+                new Operation("replace", "/Title", null, "NewBox"),
+                new Operation("replace", "/Id", null, 2)
+            };
+            var expected = new JsonPatchDocument(expectedOperations, new DefaultContractResolver());
+            _mocker.GetMock<IJsonPatchGenerator<IJsonPatchDocumentWrapper>>()
+                .Setup(m => m.Generate(first, second))
+                .Returns(new JsonPatchDocumentWrapper(expected));
+            var target = _mocker.Create<JsonPatchDocumentGeneratorService>();
+
+            // act
+            var result = target.Generate(first, second);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
