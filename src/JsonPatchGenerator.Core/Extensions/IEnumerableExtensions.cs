4240e891e4c984ad8163f40ead9863539775acd9
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-23 15:02:31 +0300

    add extension methods for calculating an enumerable object hashcode

---
+++ src/JsonPatchGenerator.Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JsonPatchGenerator.Core.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static int GetElementsHashCode<T>(this T[] array) =>
+            GetElementsHashCode(array as IEnumerable<T>);
+
+        public static int GetElementsHashCode<T>(this IEnumerable<T> enumerable) =>
+            enumerable.Aggregate(13, (total, next) => total *= 17 + (next?.GetHashCode() ?? int.MinValue));
+    }
+}
