c082be34e4653c0986015e8ab053996313f72e6a
Mikhail Podolskiy <edge769@yandex.ru>
2020-03-05 23:20:09 +0300

    add tests for registering the Json Patch Generator services

---
+++ tests/JsonPatchGenerator.AspNetCore.Tests/Tests/IServiceCollectionExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoMoqCore;
+using JsonPatchGenerator.AspNetCore.Extensions;
+using JsonPatchGenerator.AspNetCore.Services;
+using JsonPatchGenerator.AspNetCore.Services.Abstract;
+using JsonPatchGenerator.Core.Services;
+using JsonPatchGenerator.Interface.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using Xunit;
+
+namespace JsonPatchGenerator.AspNetCore.Tests.Tests
+{
+    public class IServiceCollectionExtensionsTests
+    {
+        readonly AutoMoqer _mocker = new AutoMoqer();
+
+        [Fact]
+        public void ReturnsTheSameReferenceToServiceCollection()
+        {
+            // arrange
+            var serviceCollection = _mocker.GetMock<IServiceCollection>().Object;
+
+            // act 
+            var result = serviceCollection.AddJsonPatchGenerator();
+
+            // assert
+            Assert.Same(serviceCollection, result);
+        }
+
+        [Fact]
+        public void RegistersJsonPatchDocumentGenerator() =>
+            TestIfServiceProviderAfterRegisteringJsonPatchGenerator(
+                MakeCorrectResolve<IJsonPatchGenerator<IJsonPatchDocument>, JsonPatchDocumentGeneratorService>);
+
+        [Fact]
+        public void RegistersInternalJsonPatchDocumentWrapperGenerator() =>
+            TestIfServiceProviderAfterRegisteringJsonPatchGenerator(
+                MakeCorrectResolve<IJsonPatchGenerator<IJsonPatchDocumentWrapper>, JsonPatchGeneratorService<IJsonPatchDocumentWrapper>>);
+
+        [Fact]
+        public void RegistersJsonPatchDocumentBuilderFactory() =>
+            TestIfServiceProviderAfterRegisteringJsonPatchGenerator(
+                MakeCorrectResolve<IPatchDocumentBuilderFactory<IJsonPatchDocumentWrapper>, JsonPatchDocumentBuilderFactory>);
+
+        [Fact]
+        public void RegistersTypeResolver() =>
+            TestIfServiceProviderAfterRegisteringJsonPatchGenerator(MakeCorrectResolve<ITypeResolver, DefaultTypeResolver>);
+
+        private void MakeCorrectResolve<TReq, TRes>(ServiceProvider provider) where TRes : TReq =>
+            Assert.True(provider.GetRequiredService<TReq>() is TRes);
+
+        private void TestIfServiceProviderAfterRegisteringJsonPatchGenerator(Action<ServiceProvider> assert)
+        {
+            // arrange
+            var serviceCollection = new ServiceCollection();
+
+            // act 
+            var result = serviceCollection.AddJsonPatchGenerator()
+                .BuildServiceProvider();
+
+            // assert
+            assert(result);
+        }
+    }
+}
