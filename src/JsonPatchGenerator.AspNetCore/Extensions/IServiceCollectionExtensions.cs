89fe5b8657934e610bbc6e8c8858c33fd58949ab
Mikhail Podolskiy <edge769@yandex.ru>
2020-03-05 23:20:55 +0300

    add ability to register the Json Patch Generator into the ASP.NET Core dependency injection service collection

---
+++ src/JsonPatchGenerator.AspNetCore/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using JsonPatchGenerator.AspNetCore.Services;
+using JsonPatchGenerator.AspNetCore.Services.Abstract;
+using JsonPatchGenerator.Core.Services;
+using JsonPatchGenerator.Interface.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JsonPatchGenerator.AspNetCore.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJsonPatchGenerator(this IServiceCollection serviceCollection) =>
+            serviceCollection
+                .AddScoped<ITypeResolver, DefaultTypeResolver>()
+                .AddScoped<IPatchDocumentBuilderFactory<IJsonPatchDocumentWrapper>, JsonPatchDocumentBuilderFactory>()
+                .AddScoped<IJsonPatchGenerator<IJsonPatchDocumentWrapper>, JsonPatchGeneratorService<IJsonPatchDocumentWrapper>>()
+                .AddScoped<IJsonPatchGenerator<IJsonPatchDocument>, JsonPatchDocumentGeneratorService>();
+    }
+}
