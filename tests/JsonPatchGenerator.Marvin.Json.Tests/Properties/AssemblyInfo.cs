6c89f293d09dc070b087b364c3370ead46a3919f
Mikhail Podolskiy <edge769@yandex.ru>
2020-03-19 23:21:39 +0300

    add a Marvin.Json tests project

---
+++ tests/JsonPatchGenerator.Marvin.Json.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JsonPatchGenerator.Marvin.Json.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JsonPatchGenerator.Marvin.Json.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e302ca9-7d7e-4520-b7a4-ef5f359a15d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tests/JsonPatchGenerator.Marvin.Json.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace JsonPatchGenerator.Marvin.Json.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
