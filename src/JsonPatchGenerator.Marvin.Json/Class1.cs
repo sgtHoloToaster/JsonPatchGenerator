2556467b05cead672620f9b2a6143ca6e2d11df0
Mikhail Podolskiy <edge769@yandex.ru>
2020-03-19 23:06:42 +0300

    add a project for Marvin.Json integration

---
+++ src/JsonPatchGenerator.Marvin.Json/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonPatchGenerator.Marvin.Json
+{
+    public class Class1
+    {
+    }
+}
