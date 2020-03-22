a94ff60fbdf71b5f7abafee114a4c0ac4f5c730f
Mikhail Podolskiy <edge769@yandex.ru>
2020-03-22 19:15:14 +0300

    add a default constructor

--- src/JsonPatchGenerator.Marvin.JsonPatch/JsonPatchDocumentGenerator.cs
+++ src/JsonPatchGenerator.Marvin.JsonPatch/JsonPatchDocumentGenerator.cs
@@ -1,4 +1,5 @@
-﻿using JsonPatchGenerator.Interface.Services;
+﻿using JsonPatchGenerator.Core.Services;
+using JsonPatchGenerator.Interface.Services;
 using JsonPatchGenerator.Marvin.JsonPatch.Abstract;
 using Marvin.JsonPatch;
 
@@ -7,6 +8,13 @@ namespace JsonPatchGenerator.Marvin.JsonPatch
     public class JsonPatchDocumentGenerator : IJsonPatchGenerator<IJsonPatchDocument>
     {
         readonly IJsonPatchGenerator<IJsonPatchDocumentWrapper> _patchGenerator;
+
+        public JsonPatchDocumentGenerator()
+        {
+            var typeResolver = new DefaultTypeResolver();
+            var buildersFactory = new JsonPatchDocumentBuilderFactory();
+            _patchGenerator = new JsonPatchGeneratorService<IJsonPatchDocumentWrapper>(typeResolver, buildersFactory);
+        }
 
         public JsonPatchDocumentGenerator(IJsonPatchGenerator<IJsonPatchDocumentWrapper> jsonPatchGenerator)
         {
