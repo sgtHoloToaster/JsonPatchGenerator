769db6afeebe4339cb80b9462a5225fe3fa7f7e0
Mikhail Podolskiy <edge769@yandex.ru>
2020-04-05 17:10:34 +0300

    add interfaces for creating generic patch documents

---
+++ src/JsonPatchGenerator.Interface/Services/IJsonPatchGeneratorGeneric.cs
@@ -0,0 +1,7 @@
+﻿namespace JsonPatchGenerator.Interface.Services
+{
+    public interface IJsonPatchGeneratorGeneric<T> : IJsonPatchGenerator<T>
+    {
+        T Generate<T1>(T1 first, T1 second);
+    }
+}
---
+++ src/JsonPatchGenerator.Interface/Services/IPatchDocumentBuilderFactoryGeneric.cs
@@ -0,0 +1,9 @@
+﻿using JsonPatchGenerator.Interface.Models.Abstract;
+
+namespace JsonPatchGenerator.Interface.Services
+{
+    public interface IPatchDocumentBuilderFactoryGeneric<out TPatchDocument> : IPatchDocumentBuilderFactory<TPatchDocument> where TPatchDocument : IPatchDocument 
+    {
+        IPatchDocumentBuilder<TPatchDocument> Create<T1>();
+    }
+}
