601abf39c731ad54a4b419b90a8f90d0d36988fd
Mikhail Podolskiy <edge769@yandex.ru>
2020-04-26 20:34:54 +0300

    add generic builder tests for AspNetCore

--- tests/JsonPatchGenerator.AspNetCore.Tests/Tests/JsonPatchDocumentBuilderFactoryTests.cs
+++ tests/JsonPatchGenerator.AspNetCore.Tests/Tests/JsonPatchDocumentBuilderFactoryTests.cs
@@ -1,4 +1,5 @@
-﻿using JsonPatchGenerator.Interface.Models.Abstract;
+﻿using JsonPatchGenerator.AspNetCore.Tests.Models;
+using JsonPatchGenerator.Interface.Models.Abstract;
 using JsonPatchGenerator.Interface.Services;
 using System;
 using Xunit;
@@ -22,6 +23,27 @@ namespace JsonPatchGenerator.AspNetCore.Tests.Tests
 
             // act
             var result = target.Create();
+
+            // assert
+            assert(result);
+        }
+
+        [Fact]
+        public void CanCreateGenericBuilder() =>
+            TestGenericBuilderCreate<Box>(patchDocument => Assert.NotNull(patchDocument));
+
+
+        [Fact]
+        public void CreatedGenericBuilderHasCorrectType() =>
+            TestGenericBuilderCreate<Box>(result => Assert.True(result is JsonPatchDocumentBuilder<Box>));
+
+        private void TestGenericBuilderCreate<T>(Action<IPatchDocumentBuilder<IPatchDocument>> assert)
+        {
+            // arrange
+            var target = new JsonPatchDocumentBuilderFactory();
+
+            // act
+            var result = target.Create<T>();
 
             // assert
             assert(result);
