f28bba828e2ecde9895d7982c523e8a9ab6068d7
Mikhail Podolskiy <edge769@yandex.ru>
2020-04-27 20:59:21 +0300

    add a generic wrapper for Marvin.JsonPatch document

---
+++ src/JsonPatchGenerator.Marvin.JsonPatch/JsonPatchDocumentWrapperOfT.cs
@@ -0,0 +1,12 @@
+﻿using JsonPatchGenerator.Marvin.JsonPatch;
+using Marvin.JsonPatch;
+
+namespace JsonPatchGenerator.Marvin.JsonPatch
+{
+    public class JsonPatchDocumentWrapper<T> : JsonPatchDocumentWrapper where T : class
+    {
+        public JsonPatchDocumentWrapper(JsonPatchDocument<T> jsonPatchDocument) : base(jsonPatchDocument)
+        {
+        }
+    }
+}
